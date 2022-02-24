b53c53652ca07a142a10db8257ca594cf5bba008
Rajal Prasad <rajalprasad@gmail.com>
2022-02-24 00:39:58 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect } from 'react';
+const ethers = require('ethers');
+const api = process.env.API_TOKEN;
+const provider = new ethers.providers.AlchemyProvider("homestead", api);
+
+export function App() {
+
+    const [block, setBlock] = useState();
+
+    useEffect(() => {
+        const loadBlock = async () => {
+            const response = await provider.getBlockNumber();
+
+            setBlock(response);
+        }
+        loadBlock();
+    }, []);
+
+    return (
+        <div>
+            {block}
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import reactDom from "react-dom";
+import { App } from "./App";
+
+
+const app = document.getElementById("app");
+reactDom.render(<App />, app);
